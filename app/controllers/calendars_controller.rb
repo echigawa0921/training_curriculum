99e76c7545e8949b850b8f1ce5ec93b3cfc507ee
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-09-29 22:20:42 +0900

    Update calendars_controller.rb

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,11 +12,12 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  private
+private
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
   end
+
 
   def getWeek
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
